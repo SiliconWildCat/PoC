cc5e2ad2af9dbb4a0def78da6544ee13e7e08088
ww8007 <wshmin@naver.com>
2021-07-09 16:48:41 +0900

    [feat/front] add-input, number

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [number, onChangeNumber] = useState(0);
+  const [text, onChangeText] = useState('123123');
+  const onClickAdd = () => {
+    onChangeNumber(number + 1);
+  };
+  const onClickMinus = () => {
+    onChangeNumber(number - 1);
+  };
+  const ChangeText = (e) => {
+    onChangeText(e.target.value);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div style={{ margin: '200px' }}>{number}</div>
+      <button onClick={onClickAdd}>+1</button>
+      <button onClick={onClickMinus}>-1</button>
+      <input type="text" value={text} onChange={ChangeText} />
+      <div>{text}</div>
     </div>
   );
 }
