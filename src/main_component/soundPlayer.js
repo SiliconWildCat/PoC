05690be30121eb6ed1a9a4c82eae9c52cbcf8e2d
Dudu-CNU <k0s0a7@gmail.com>
2021-07-12 20:47:47 +0900

    [feat] update-player

--- src/main_component/soundPlayer.js
+++ src/main_component/soundPlayer.js
@@ -2,10 +2,8 @@ import React, { useState, useEffect } from 'react';
 import AudioPlayer from 'react-h5-audio-player';
 import axios from 'axios';
 import styles from './styles.css';
-import audio1 from './music/What_Do_I_Call_You.mp3';
-import audio2 from './music/in501.wav';
-import audio3 from './music/little_star_generated_e2e.wav';
-import audio4 from './music/result2.wav';
+
+import test_audio1 from './music/in501.wav';
 
 const Player = () => {
   const [users, setUsers] = useState(null);
@@ -21,61 +19,53 @@ const Player = () => {
       setLoading(true);
 
       const response = await axios.get(
-        'https://fastcampus2022.s3.ap-northeast-2.amazonaws.com/Ed+Sheeran+Bad+Habits+%5BOfficial+Acoustic+Video%5D+(2)+(2).mp3'
+        'https://storage.googleapis.com/siliconwildcat_poc_bucket/music/IU_Secret_Garden.mp3'
       );
-      const { data } = response;
-      setMp3(response.data); // 데이터는 response.data 안에 들어있습니다.
-      setError(response);
-      // setMp3(JSON.parse(data));
-      setUsers(JSON.stringify(response));
-    } catch (e) {
+      setMp3(response.config.url); // 데이터는 response.data 안에 들어있습니다.
+      console.log(response);
+
+      } catch (e) {
       setError(e);
     }
     setLoading(false);
   };
+
   const onClickDown = () => {
     fetchUsers();
-  };
+  }; 
 
   return (
     <div>
-      {/* {loading && (
-        <AudioPlayer className={styles} onPlay={(e) => console.log('onPlay')} />
-      )} */}
-      <button onClick={onClickDown}>다운 로드</button>
-      {console.log('hhih', mp3)}
-      {!loading && users && (
-        <AudioPlayer
-          className={styles}
-          src="https://fastcampus2022.s3.ap-northeast-2.amazonaws.com/Ed+Sheeran+Bad+Habits+%5BOfficial+Acoustic+Video%5D+(2)+(2).mp3"
-          onPlay={(e) => console.log('onPlay')}
-        />
-      )}
-      <br />
-      {mp3 && (
+    <button id="down_btn" onClick={onClickDown}>다운로드</button><br />
+       
+       {mp3 && (
+        <div> 
+        다운로드 버튼 플레이어
         <AudioPlayer
           className={styles}
           src={mp3}
           onPlay={(e) => console.log('onPlay')}
         />
+        </div>
       )}
       <br />
-      {error && (
-        <AudioPlayer
+      src 링크 
+      <AudioPlayer
           className={styles}
-          src={error}
+          src="https://fastcampus2022.s3.ap-northeast-2.amazonaws.com/Ed+Sheeran+-+Bad+Habits+%5BOfficial+Video%5D.mp3"
           onPlay={(e) => console.log('onPlay')}
         />
-      )}
       <br />
+      로컬
       <AudioPlayer
-        className={styles}
-        src={audio4}
-        onPlay={(e) => console.log('onPlay')}
-      />
+          className={styles}
+          src={test_audio1}
+          onPlay={(e) => console.log('onPlay')}
+        />
       <br />
     </div>
   );
 };
+
 
 export default Player;
