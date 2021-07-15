630f93c88c8fc586acdbc6ad76dcae7f5505cd9c
ww8007 <wshmin@naver.com>
2021-07-15 19:15:30 +0900

    feat : add-pages

---
+++ src/pages/MusicPage.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import DaysContainer from '../containers/DaysContainer';
+
+export const MusicPage = () => {
+  // 버튼
+  return <DaysContainer></DaysContainer>;
+  //
+};
