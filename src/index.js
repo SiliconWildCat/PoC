53db3b7e3368f14aaf6a11ca5f57ed19d935b7a8
ww8007 <wshmin@naver.com>
2021-07-15 18:54:24 +0900

    fix : Router -> Hash

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { HashRouter } from 'react-router-dom';
 //import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <HashRouter>
     <App />
-  </React.StrictMode>,
+  </HashRouter>,
   document.getElementById('root')
 );
 
